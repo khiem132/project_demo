ba2fd77c154ef9bc5d3ffe180c038bf23563deed
khiem132 <khiem132@gmail.com>
2025-03-11 20:25:40 +0700

    all

---
+++ Models/ModelViews/CategoryView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP_5_Upload.Models.ModelViews
+{
+    public class CategoryView
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+    }
+}
